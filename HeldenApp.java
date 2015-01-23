ca2fcc61601766cbeb3be5e4cb8743f56f2e3d9c
WasFuerNutzerName <toni.stella@gmx.de>
2015-01-23 13:59:20 +0100

    Update HeldenApp.java

--- HeldenApp.java
+++ HeldenApp.java
@@ -1,15 +1,41 @@
-public class HeldenApp {
+import java.awt.Button;
+import java.awt.event.*;
+import java.beans.EventHandler;
+
+import javax.swing.*;
+
+
+public class HeldenApp implements ActionListener{
+	
 	public static void main(String... args){
 		
-		
-		Held shaggy = new Held("Shaggy", 2500, 500, 40, false, new Waffe("Bloodscream", 350, "Zweihand-Axt"));
-		Held frank = new Held("Frank White", 1500, 850, 10, true, new Waffe("Ashbringer", 150, "Zweihand-Schwert"));
+		Held shaggy = new Held("Shaggy", 2500, 700, 40, false, new Waffe("Ashbringer", 150, "Zweihand-Schwert"), 1);
+		Held frank = new Held("Frank White", 2000, 850, 40, true, new Waffe("Blutschrei", 350, "Zweihand-Axt"), 1);
 		shaggy.getWaffe();
 		frank.getWaffe();
 		
-		kampf(frank, shaggy, 3000);
 		
+		
+		kampf(frank, shaggy, 2000);
 	}
+	
+	public void los(){
+		JFrame frame = new JFrame();
+
+		JButton button = new JButton("Kampf starten");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(button);
+		
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		button.setSize(150, 150);
+		
+		button.addMouseListener(new MouseAdapter(){});
+	}
+	
+	
 	public static void kampf(Held angreifer, Held verteidiger, int speed) {
 		
 		String nameAngreifer = angreifer.getName();
@@ -18,36 +44,49 @@ public class HeldenApp {
 		// Text des Kampfgeschehen
 		System.out.println( nameAngreifer + " greift " + nameVerteidiger + " an! \n\n");
 		
-		
-		
 		//Die Helden begrüßen sich
 		System.out.println(angreifer.vorstellen() + "\n" + verteidiger.vorstellen());
-		
-		
 		
 		//Die Helden kämpfen
 		while (angreifer.getLifepoints() > 0 && verteidiger.getLifepoints() > 0) {
 				
 			verzoegere(speed);
-			System.out.println(nameAngreifer + " schlägt " + nameVerteidiger + "!\n"
+			
+			System.out.println(nameAngreifer + " greift mit " + angreifer.getWaffe().getWaffenName() 
+								+ " (" + angreifer.getWaffe().getWaffenArt() + ") an."); 
+
+			System.out.print(nameAngreifer + " schlägt " + nameVerteidiger + "!\n"
 					+ nameVerteidiger + " erleidet Schaden von " 
 					+ angreifer.schlagen(verteidiger) + " und hat noch "
 					+ verteidiger.getLifepoints() + " Lebenspunkte. \n\n");
 			
-			System.out.println(nameAngreifer + " greift mit " + angreifer.getWaffe().getWaffenName() 
-								+ " (" + angreifer.getWaffe().getWaffenArt() + ") an." 
-								+ nameVerteidiger + " greift mit " + verteidiger.getWaffe().getWaffenName() 
-								+ " (" + verteidiger.getWaffe().getWaffenArt() + ") an.");
+			speed = 3500;
+			verzoegere(speed);
+			if (verteidiger.getLifepoints() < 500 && verteidiger.getHealpot() > 0) {
+				verteidiger.getHealpot();
+				verteidiger.setLifepoints(+500);
+				System.out.println(nameVerteidiger + ": Ich brauche schnell einen Trank");
+				verteidiger.setHealpot(0);
+			}
+			
 			if (verteidiger.getLifepoints() > 0) {
 				
-			
+				System.out.println(nameVerteidiger + " greift mit " + verteidiger.getWaffe().getWaffenName() 
+									+ " (" + verteidiger.getWaffe().getWaffenArt() + ") an."); 
 			// Verteidiger schlägt zurück
 				System.out.println(nameVerteidiger + " schlägt " + nameAngreifer + "!\n"
 						+ nameAngreifer + " erleidet Schaden von " 
 						+ verteidiger.schlagen(angreifer) + " und hat noch "
 						+ angreifer.getLifepoints() + " Lebenspunkte. \n\n");
-		}
+			}
+			 if (angreifer.getLifepoints() < 500 && angreifer.getHealpot() > 0) {
+				angreifer.getHealpot();
+				angreifer.setLifepoints(+500);
+				System.out.println(nameAngreifer + ": Ich brauche schnell einen Trank");
+				angreifer.setHealpot(0);
+			}	
 	}
+		
 		//Ein Held stirbt
 		if (angreifer.getLifepoints() > 0) {
 			System.out.println( verteidiger.sterben());
@@ -62,4 +101,9 @@ public class HeldenApp {
 			Thread.currentThread().interrupt();
 		}
 	}
+		
+		public void actionPerformed(ActionEvent arg0, Held angreifer, Held verteidiger, int speed) {
+			HeldenApp.kampf(angreifer, verteidiger, speed);
+			
+		}
 }
